bd25333e115dd55121149396c438da1d715d8b14
Adam Terebiński <aterebinski@wp.pl>
2020-10-07 15:55:33 +0200

    Walidacja, partiale, usuwanie

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,10 +10,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    session[:author] = @post.author
-    flash[:notice] = "Rekord dodany poprawnie!"
-    redirect_to posts_path
+    if @post.save
+      session[:author] = @post.author
+      flash[:notice] = "Rekord dodany poprawnie!"
+      redirect_to posts_path
+    else
+      render action: "new"
+    end
   end
 
   def edit
@@ -22,9 +25,12 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update!(post_params)
-    flash[:notice] = "Wpis został zaktualizowany"
-    redirect_to posts_path
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Wpis został zaktualizowany"
+      redirect_to posts_path
+    else
+      render action: "edit"
+    end
   end
 
   def show
@@ -32,6 +38,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   def post_params
